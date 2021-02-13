b062a9411d62999d53313c8887bd233b4885c5d4
ThaGrza <andrewgriswold7@gmail.com>
2021-02-12 20:25:34 -0500

    header

--- App.tsx
+++ App.tsx
@@ -1,13 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Header from './src/components/Header'
 
-export default function App() {
+
+export default function Container() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+      <Header />
   );
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,34 @@
+import React from 'react'
+import { View, StyleSheet, Text } from 'react-native';
+
+
+const Header = () => {
+  return(
+    <View style={styles.headerContainer}>
+      <Text style={styles.headerText}>GUMBALL MOVIES</Text>
+      <Text style={styles.headerSubText}>Randomize Your Movie Night</Text>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  headerContainer: {
+    display: "flex",
+    alignItems: "center",
+    backgroundColor: "pink",
+    paddingTop: 100,
+    paddingBottom: 20,
+    justifyContent: "center",
+  },
+  headerText: {
+    fontWeight: "bold",
+    fontSize: 36,
+    paddingBottom: 10 
+  },
+  headerSubText: {
+    fontWeight: "400",
+    fontSize: 18
+  }
+})
+
+export default Header

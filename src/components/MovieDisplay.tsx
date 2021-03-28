5921237f20cbef90b6328c740458e643e1002a43
ThaGrza <andrewgriswold7@gmail.com>
2021-03-28 17:29:45 -0400

    put api call into display function + set data states

--- src/components/MovieDisplay.tsx
+++ src/components/MovieDisplay.tsx
@@ -1,22 +1,47 @@
 import React, { useState, useEffect } from 'react';
 import {View, Image, Text, StyleSheet } from 'react-native';
 import GenreSelector from './GenreSelector';
-
+import Axios from 'axios';
 // ADD TO IMAGE SOURCE + IMG STATE FOR POSTER
-const movieImg: string = "https://image.tmdb.org/t/p/w500/"
+const baseImageUrl: any = "https://image.tmdb.org/t/p/w500/"
+const apiKey = "5f9630b664fee3f1c639e0ae94090867";
+const baseUrl: string = "https://api.themoviedb.org/3/discover/movie?api_key=";
+const genreUrl: string = "&with_genres=";
+
+
+
 
 const MovieDisplay = (movie: object) => {
-  const [movieId, setMovieId] = useState();
   const [title, setTitle] = useState();
   const [movieImg, setMovieImg] = useState();
   const [overview, setOverview] = useState();
   const [releaseDate, setReleaseDate] = useState();
 
+
+  const movieJeeves = (movieId: number) => {
+    let query: any = baseUrl + apiKey + genreUrl + movieId;
+    let movie = {};
+    Axios.get(query)
+      .then(res => {
+        movie = res.data.results[Math.floor(Math.random() * res.data.results.length)];
+        setTitle(movie.original_title);
+        setMovieImg(baseImageUrl + movie.poster_path);
+        setOverview(movie.overview);
+        setReleaseDate(movie.release_date);
+      })
+      .catch(err => {
+        console.log(err);
+      }) 
+  }
+
+
   return(
     <View style = {styles.movieImage} >
       <Image style = {styles.movieImage} source={{uri: movieImg }} />
       <Text style = {styles.movieInfo} >{title}</Text>
-      <GenreSelector onPress={value => setMovieId(value)} />
+      <Text style = {styles.movieInfo} >{releaseDate}</Text>
+      <Text style = {styles.movieInfo} >{overview}</Text>
+      <GenreSelector onPress={value => movieJeeves(value)} />
     </View>
   )
 };
--- src/components/Moviejeeves.tsx
+++ src/components/Moviejeeves.tsx
@@ -1,41 +1,34 @@
 import React from 'react';
 import { View } from 'react-native';
 import Axios from 'axios';
-import MovieDisplay from './MovieDisplay';
 // import { API_KEY } from '@env';
-
-// DELETE BEFORE COMMITING
-// DELETE BEFORE COMMITING
-// DELETE BEFORE COMMITING
-// DELETE BEFORE COMMITING
-// DELETE BEFORE COMMITING
 const apiKey = "5f9630b664fee3f1c639e0ae94090867";
-// DELETE BEFORE COMMITING
-// DELETE BEFORE COMMITING
-// DELETE BEFORE COMMITING
-// DELETE BEFORE COMMITING
-// DELETE BEFORE COMMITING
-
-
 const baseUrl: string = "https://api.themoviedb.org/3/discover/movie?api_key=";
 const genreUrl: string = "&with_genres=";
+
+
+
+
+
+
 const MovieJeeves = (id: number) => {
   let query: any = baseUrl + apiKey + genreUrl + id;
   let movie = {};
   Axios.get(query)
     .then(res => {
       movie = res.data.results[Math.floor(Math.random() * res.data.results.length)];
-      MovieDisplay(movie);
+      console.log(movie);
+      return movie; 
     })
     .catch(err => {
       console.log(err);
     });
-  MovieDisplay(movie);
 
 
   return(
     <View/>
   )
 }
+
 export default MovieJeeves;
 
